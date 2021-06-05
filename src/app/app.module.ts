a164f199b5fa11e4ea9621bc40c739d03d4e98d5
Bohdan566 <bohdan55566@gmail.com>
2021-06-05 23:52:10 +0300

    lesson_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import {FormsModule} from "@angular/forms";
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/db/data.ts
@@ -0,0 +1,8 @@
+export let users = [
+  {name : 'Bohdan', age : 19, status : true},
+  {name : 'Ivan', age : 34, status : true},
+  {name : 'Dimas', age : 23, status : true},
+  {name : 'Vova', age : 56, status : false},
+  {name : 'Serhiy', age : 24, status : true},
+  {name : 'Boba', age : 20, status : false},
+];
